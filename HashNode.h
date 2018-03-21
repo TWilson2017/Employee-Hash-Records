76b8f46a329c981319ef4e9bc21efb77d3640362
Tishauna Wilson <25163556+TWilson2017@users.noreply.github.com>
2018-03-20 20:30:23 -0400

    Update HashNode.h

--- HashNode.h
+++ HashNode.h
@@ -19,19 +19,19 @@ class HashNode
 		HashNode(char last_name1[15], char first_name1[15], int account_num1, int month1, int day1,
 		         int year1, double annual_salary1, char dept_code1, char phone_number1[12])
 		{
-			//Initialize Variables to Inserted Variables
+		    //Initialize Variables to Inserted Variables
 		    strncpy(last_name, last_name1, 15);
-            last_name[14] = '\0';
-			strncpy(first_name, first_name1, 15);
-            first_name[14] = '\0';
-			account_num = account_num1;
-			month = month1;
-			day = day1;
-			year = year1;
-			annual_salary = annual_salary1;
-			dept_code = dept_code1;
-			strncpy(phone_number, phone_number1, 12);
-            phone_number[11] = '\0';
+                    last_name[14] = '\0';
+		    strncpy(first_name, first_name1, 15);
+                    first_name[14] = '\0';
+		    account_num = account_num1;
+		    month = month1;
+		    day = day1;
+		    year = year1;
+		    annual_salary = annual_salary1;
+		    dept_code = dept_code1;
+		    strncpy(phone_number, phone_number1, 12);
+                    phone_number[11] = '\0';
 		}//2nd Constructor HashNode
 };//HashNode
 #endif
